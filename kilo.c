ac99cd2fca2becd8bb32ad33be511c9b82ab0306
Lu Zhu <misterzhu@gmail.com>
2019-08-18 15:24:28 -0700

    Home and End keys

--- kilo.c
+++ kilo.c
@@ -26,6 +26,8 @@ enum editorKey {
   ARROW_RIGHT,
   ARROW_UP,
   ARROW_DOWN,
+  HOME_KEY,
+  END_KEY,
   PAGE_UP,
   PAGE_DOWN
 };
@@ -116,14 +118,19 @@ int editorReadKey() {
     // If either of these timeout, assume the user just pressed Esc
     if (read(STDIN_FILENO, &seq[0], 1) != 1) return '\x1b';
     if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
-    // Convert arrows to the corresponding WASD character
+    // "Home" and "End" sequences depend on the OS and terminal emulator, so we
+    // need to handle all of the different sequences
     if (seq[0] == '[') {
       if (seq[1] >= '0' && seq[1] <= '9') {
         if (read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
         if (seq[2] == '~') {
           switch (seq[1]) {
+          case '1': return HOME_KEY;
+          case '4': return END_KEY;
           case '5': return PAGE_UP;
           case '6': return PAGE_DOWN;
+          case '7': return HOME_KEY;
+          case '8': return END_KEY;
           }
         }
       } else {
@@ -132,7 +139,14 @@ int editorReadKey() {
         case 'B': return ARROW_DOWN;
         case 'C': return ARROW_RIGHT;
         case 'D': return ARROW_LEFT;
+        case 'H': return HOME_KEY;
+        case 'F': return END_KEY;
         }
+      }
+    } else if (seq[0] == 'O') {
+      switch (seq[1]) {
+      case 'H': return HOME_KEY;
+      case 'F': return END_KEY;
       }
     }
 
@@ -323,6 +337,14 @@ void editorProcessKeypress() {
     write(STDOUT_FILENO, "\x1b[2J", 4);
     write(STDOUT_FILENO, "\x1b[H", 3);
     exit(0);
+    break;
+
+  case HOME_KEY:
+    E.cx = 0;
+    break;
+
+  case END_KEY:
+    E.cx = E.screencols - 1;
     break;
 
   case PAGE_UP:
