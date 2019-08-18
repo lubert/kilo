53ea89d8445bddb8aef1f0838c2eacdfa59008f4
Lu Zhu <misterzhu@gmail.com>
2019-08-17 18:48:46 -0700

    Refactor keyboard input

--- kilo.c
+++ kilo.c
@@ -75,23 +75,40 @@ void enableRawMode() {
   if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
 
+/**
+ * Waits for one keypress and returns it
+ */
+char editorReadKey() {
+  int nread;
+  char c;
+  while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+    if (nread == -1 && errno != EAGAIN) die("read");
+  }
+  return c;
+}
+
+/*** input ***/
+
+/**
+ * Handles a keypress
+ */
+void editorProcessKeypress() {
+  char c = editorReadKey();
+
+  switch (c) {
+  case CTRL_KEY('q'):
+    exit(0);
+    break;
+  }
+}
+
 /*** init ***/
 
 int main() {
   enableRawMode();
 
   while (1) {
-    char c = '\0';
-    // Cygwin returns EAGAIN instead of returning 0 when read() times out, so we ignore EAGAIN
-    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
-    // iscntrl, from ctype, tests whether a character is a control character
-    // \r is added because we disabled output processing which automatically inserts it with newlines
-    if (iscntrl(c)) {
-      printf("%d\r\n", c);
-    } else {
-      printf("%d ('%c')\r\n", c, c);
-    }
-    if (c == CTRL_KEY('q')) break;
+    editorProcessKeypress();
   }
 
   return 0;
