a2ff8c379dc6d9538d2014bce6344761bd575d08
Lu Zhu <misterzhu@gmail.com>
2019-09-11 09:42:20 -0700

    Insert ordinary characters

--- kilo.c
+++ kilo.c
@@ -288,6 +288,28 @@ void editorAppendRow(char *s, size_t len) {
   E.numrows++;
 }
 
+void editorRowInsertChar(erow *row, int at, int c) {
+  // at is allowed to go one char past the end to allow insertion
+  if (at < 0 || at > row->size) at = row->size;
+  // adding 2 because we have to make room for the null byte
+  row->chars = realloc(row->chars, row->size + 2);
+  // memmove() is like memcpy(), but is safe when the src/dest are the same
+  memmove(&row->chars[at + 1], &row->chars[at], row->size - at + 1);
+  row->size++;
+  row->chars[at] = c;
+  editorUpdateRow(row);
+}
+
+/*** editor operations ***/
+
+void editorInsertChar(int c) {
+  if (E.cy == E.numrows) {
+    editorAppendRow("", 0);
+  }
+  editorRowInsertChar(&E.row[E.cy], E.cx, c);
+  E.cx++; // Move cursor after insertion
+}
+
 /*** file i/o ***/
 
 void editorOpen(char *filename) {
@@ -597,6 +619,9 @@ void editorProcessKeypress() {
   case ARROW_LEFT:
   case ARROW_RIGHT:
     editorMoveCursor(c);
+    break;
+  default:
+    editorInsertChar(c);
     break;
   }
 }
