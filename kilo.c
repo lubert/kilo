1a3ffdf5c32fe58e908e15a3facc026dfeee8c13
Lu Zhu <misterzhu@gmail.com>
2019-09-29 13:59:48 -0700

    Quit confirmation

--- kilo.c
+++ kilo.c
@@ -26,6 +26,8 @@
 
 #define KILO_VERSION "0.0.1"
 #define KILO_TAB_STOP 4
+#define KILO_QUIT_TIMES 3
+
 // 0x1f is 00011111
 // Masking the upper 3 bits effectively does what the Ctrl key does
 // ASCII seems to have been designed this way on purpose, and also
@@ -640,6 +642,8 @@ void editorMoveCursor(int key) {
  * Handles a keypress
  */
 void editorProcessKeypress() {
+  static int quit_times = KILO_QUIT_TIMES;
+
   int c = editorReadKey();
 
   switch (c) {
@@ -648,6 +652,12 @@ void editorProcessKeypress() {
     break;
 
   case CTRL_KEY('q'):
+    if (E.dirty && quit_times > 0) {
+      editorSetStatusMessage("WARNING!!! File has unsaved changes. "
+                             "Press Ctrl-Q %d more times to quit.", quit_times);
+      quit_times--;
+      return;
+    }
     write(STDOUT_FILENO, "\x1b[2J", 4);
     write(STDOUT_FILENO, "\x1b[H", 3);
     exit(0);
@@ -706,6 +716,8 @@ void editorProcessKeypress() {
     editorInsertChar(c);
     break;
   }
+
+  quit_times = KILO_QUIT_TIMES;
 }
 
 /*** init ***/
